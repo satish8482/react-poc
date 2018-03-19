58496665202c39668dc42be519257c7451805f3a
satish8384 <satish8482@gmail.com>
2018-03-19 11:27:20 +0530

    poc

--- src/pages/City1.js
+++ src/pages/City1.js
@@ -5,7 +5,7 @@ const City1 = () => (
   <div>
     <img src={Banglore} width="100%" height="100%"/>
     <h2>This is Bangalore and  has many tourist places which will attract people all over the world</h2>
-    <h3>Banglore is Green city</h3>
+    <h3>Banglore is Green city.</h3>
       <p></p>
   </div>
 );
